220c0da96907c93c02aea991016988a1f01ad95d
Noah Hernandez <63211322+oahnh@users.noreply.github.com>
2024-11-05 07:22:12 -0800

    add post struct, modularize a bit

--- bot/pkg/bsky/bsky.go
+++ bot/pkg/bsky/bsky.go
@@ -6,9 +6,21 @@ import (
 	"io"
 	"net/http"
 	"strings"
+
+	"github.com/togdon/reply-bot/gsheets"
 )
 
-func main() {
+type Post struct {
+	URI         string                 `json:"uri"`
+	CID         string                 `json:"cid"`
+	Author      map[string]interface{} `json:"author"`
+	Record      map[string]interface{} `json:"record"`
+	ReplyCount  int                    `json:"replyCount"`
+	RepostCount int                    `json:"reposeCount"`
+	QuoteCount  int                    `json:"quoteCount"`
+}
+
+func FetchPosts(client *gsheets.GSheetsClient) {
 	BSKY_API_URL := "https://public.api.bsky.app/xrpc"
 
 	BSKY_SEARCH_ENDPOINT := "app.bsky.feed.searchPosts"
@@ -34,9 +46,9 @@ func main() {
 	}
 
 	type SearchResponse struct {
-		Cursor    string //TODO pagination, no idea how it works yet
-		HitsTotal int
-		Posts     []map[string]interface{} //rest of the struct is here https://docs.bsky.app/docs/api/app-bsky-feed-search-posts#responses
+		Cursor    string `json:"cursor"` //TODO pagination, no idea how it works yet
+		HitsTotal int    `json:"hitsTotal"`
+		Posts     []Post `json:"posts"` //rest of the struct is here https://docs.bsky.app/docs/api/app-bsky-feed-search-posts#responses
 	}
 
 	var search_hits SearchResponse
@@ -50,32 +62,43 @@ func main() {
 	for _, post := range search_hits.Posts {
 		//TODO more specific logic to filter bots / low engagement posts / low follower authors?
 
-		fmt.Println("URI: ", post["uri"]) //TODO not sure how to go from URI here to a browser loadable url?
+		// fmt.Println("URI: ", post["uri"]) //TODO not sure how to go from URI here to a browser loadable url?
 		// approach here: https://github.com/bluesky-social/atproto/discussions/2523#discussioncomment-9552109
 		// [noah] note: rather than using the DID, we use the author handle which makes the url more concise (in most cases) and readable
 
-		uri, ok := post["uri"].(string)
-		if !ok {
-			fmt.Printf("Error for uri %d: could not cast to string", post["uri"])
+		url, err := generateBskyUrl(post)
+		if err != nil {
+			fmt.Printf("error generating bsky url for uri %v", err)
 		}
 
-		author, ok := post["author"].(map[string]interface{})
-		if !ok {
-			fmt.Printf("error casting author to string map")
-		}
-
-		handle, ok := author["handle"].(string)
-		if !ok {
-			fmt.Printf("error casting handle to string")
-		}
-
-		parts := strings.Split(uri, "/")
-		rkey := parts[len(parts)-1]
-
-		fmt.Printf("Associated URL: https://bsky.app/profile/%s/post/%s\n", handle, rkey)
-		// postUrl := fmt.Sprintf("https://bsky.app/profile/%s/post/%s\n", did, rkey)
+		fmt.Printf("Associated URL: %s\n", url)
 
 		//TODO write to the google sheet where responses can be generated?
 
 	}
+}
+
+func generateBskyUrl(post Post) (string, error) {
+	uri := post.URI
+	handle, ok := post.Author["handle"]
+	if !ok {
+		return "", fmt.Errorf("author handle invalid")
+	}
+
+	rkey, err := extractRKey(uri)
+	if err != nil {
+		return "", fmt.Errorf("failed to extract rkey for post: %w", err)
+	}
+
+	return fmt.Sprintf("https://bsky.app/profile/%s/post/%s", handle, rkey), nil
+
+}
+
+func extractRKey(uri string) (string, error) {
+	parts := strings.Split(uri, "/")
+	if len(parts) < 2 {
+		return "", fmt.Errorf("invalid uri format")
+	}
+
+	return parts[len(parts)-1], nil
 }
